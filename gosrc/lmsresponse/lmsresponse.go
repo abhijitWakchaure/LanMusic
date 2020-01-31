15c564b172670cb3ab3f6a90787b2f3fde0ec16d
Abhijit Wakchaure <abhijitwakchaure.2014@gmail.com>
2020-02-01 03:08:27 +0530

    added: GetResponseBytes to standardize server response

--- gosrc/lmsresponse/lmsresponse.go
+++ gosrc/lmsresponse/lmsresponse.go
@@ -1,8 +1,36 @@
 package lmsresponse
 
-// IResponse ...
-type IResponse struct {
-	Status  string      `json:"status"`
-	Message string      `json:"message"`
+import (
+	"encoding/json"
+)
+
+// constants ...
+const (
+	SUCCESS = "success"
+	ERROR   = "error"
+)
+
+// Response is a generic server response
+type Response struct {
+	Status  string      `json:"status" binding:"required"`
+	Message string      `json:"message" binding:"required"`
 	Data    interface{} `json:"data,omitempty"`
+}
+
+// GetResponseBytes ...
+func GetResponseBytes(status string, message string, data interface{}) []byte {
+	response := Response{
+		Status:  status,
+		Message: message,
+		Data:    data,
+	}
+	res, err := json.Marshal(response)
+	if err != nil {
+		res, _ = json.Marshal(Response{
+			Status:  "error",
+			Message: "Unable to marshal server response",
+		})
+		return res
+	}
+	return res
 }
