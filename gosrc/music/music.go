58ab2d816288f6d167d6c3008c562cb49ef928e3
apadwal <apadwal@tibco.com>
2020-01-31 19:14:00 +0530

    added: full song path in response

--- gosrc/music/music.go
+++ gosrc/music/music.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 
 	"github.com/abhijitWakchaure/lanmusic/gosrc/logger"
 )
@@ -25,6 +26,28 @@ type MObject struct {
 	Name  string `json:"name"`
 	Size  int64  `json:"size"`
 	IsDir bool   `json:"isDir"`
+	Path  string `json:"path"`
+}
+
+func songPath(files []os.FileInfo, mlist *MList, musicroot string) error {
+	for _, f := range files {
+		if f.IsDir() {
+			curRoot := path.Join(musicroot, f.Name())
+			files, err := ioutil.ReadDir(curRoot)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = songPath(files, mlist, curRoot)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		if !f.IsDir() {
+			md := MObject{Name: f.Name(), Size: f.Size(), IsDir: f.IsDir(), Path: path.Join(musicroot, f.Name())}
+			mlist.Filenames = append(mlist.Filenames, md)
+		}
+	}
+	return nil
 }
 
 func listDir() MList {
@@ -36,11 +59,11 @@ func listDir() MList {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, f := range files {
-		md := MObject{Name: f.Name(), Size: f.Size(), IsDir: f.IsDir()}
-		mlist.Filenames = append(mlist.Filenames, md)
-
+	err = songPath(files, &mlist, MUSICROOT)
+	if err != nil {
+		log.Fatal(err)
 	}
+
 	return mlist
 }
 
